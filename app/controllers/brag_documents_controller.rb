214f86188d6be8cf1967fcf638aff265d9093a16
kritsada.ma <kritsada.ma@kkumail.com>
2025-08-16 20:20:51 +0700

    Refactor quest views to enhance layout and display of active and completed quests

--- app/controllers/brag_documents_controller.rb
+++ app/controllers/brag_documents_controller.rb
@@ -1,5 +1,6 @@
 class BragDocumentsController < ApplicationController
   def show
     @completed_quests = Quest.where(status: true).order(updated_at: :desc)
+    @uncompleted_quests = Quest.where(status: false).order(updated_at: :desc)
   end
 end
