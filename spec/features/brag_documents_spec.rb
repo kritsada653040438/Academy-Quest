81424f5fa805b6e305b2a8a6b509916e1f387ef9
kritsada.ma <kritsada.ma@kkumail.com>
2025-08-17 19:21:31 +0700

    Update Brag Document view and tests for improved clarity and consistency

--- spec/features/brag_documents_spec.rb
+++ spec/features/brag_documents_spec.rb
@@ -2,7 +2,19 @@
 require 'rails_helper'
 
 RSpec.feature "BragDocuments", type: :feature do
-  scenario "user visits brag document page and sees quests", js: true do
+  scenario "user visits brag document page" do
+    visit brag_document_path
+    expect(page).to have_content("My Brag Document")
+  end
+
+  scenario "user can go back to Quest page" do
+    visit brag_document_path
+    click_link "Back to Quest Hub"
+    expect(page).to have_content("Kritsada Mahanam")
+    expect(page).to have_content("My Academy Quests")
+  end
+
+  scenario "user visits brag document page and sees completed quests" do
     # สร้าง test data
     completed_quest = Quest.create!(title: "Finished Quest", status: true, updated_at: 2.days.ago)
 
@@ -12,6 +24,4 @@ RSpec.feature "BragDocuments", type: :feature do
     # เช็คว่าเนื้อหามีครบ
     expect(page).to have_content("Finished Quest")
   end
-
-  
 end
